27dab4c3abbc034969fce7c83a668a26e505162f
kikigigi <momokocute1@gmail.com>
2022-07-11 10:14:19 +0100

    Added database

--- app.py
+++ app.py
@@ -1,27 +1,71 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+import sqlite3
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-secret_key = os.getenv('SECRET_KEY')
 app = Flask(__name__)
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
 api = Api(app)
+jwt = JWT(app, authenticate, identity)
 
-class Store(Resource):
 
-    def get(self, name):
-        return {'name': name}
+items = []
 
-class Stores(Resource):
+class Items(Resource):
 
     def get(self):
-        stores = []
-        return {'stores': stores}
+        return {'items': items}
 
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Stores, '/stores')
+class Item(Resource):
 
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help='This filed can not be blanked.')
+
+
+    @classmethod
+    def find_item(cls, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is not None:
+            print(f"get item {item['name']}")
+        else:
+            print('Item is None')
+        return item
+
+    @jwt_required()
+    def get(self, name):
+        item = Item.find_item(name)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        item = Item.find_item(name)
+        data = Item.parser.parse_args()
+        if item is None:
+            items.append({'name': name, 'price': data['price']})
+            return {'message': f"{items[-1]['name']} created", 'item': items[-1]}, 201 # created
+        return {'message': f"{name} has existed."}, 400 # bad request
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': f"{name} deleted"}
+
+    def put(self, name):
+        item = Item.find_item(name)
+        data = Item.parser.parse_args()
+        if item:
+            item.update(data)
+            return {'message': f"{name} price changed."}
+        items.append({'name': name, 'price': data['price']})
+        return {'message': f"{name} created!"}, 201
+
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
 
 
 if __name__ == '__main__':
---
+++ security.py
@@ -0,0 +1,13 @@
+from user import User
+import hmac
+
+
+def authenticate(username, password):
+    user = User.find_user_by_username(username)
+    if user and hmac.compare_digest(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return User.find_user_by_id(user_id)
